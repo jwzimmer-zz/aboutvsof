cf07424b2de1d5a177067de31e39bd5b8aa95336
jwzimmer <jzimmer1@uvm.edu>
2021-01-12 18:53:22 -0500

    tried extremely simple linear regression approach, hot garbage

--- projcam_conf_gettext.py
+++ projcam_conf_gettext.py
@@ -9,8 +9,14 @@ Created on Mon Jan 11 14:54:31 2021
 from bs4 import BeautifulSoup
 import os
 import json
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
 import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
 
 # === === Helper functions === ===
 def write_json(data, filename):
@@ -34,54 +40,117 @@ class CamelotConfTranscript():
             soup = BeautifulSoup(open(DirName+f,encoding="ISO-8859-1"),features="lxml")
             allps = soup.find_all('p',class_="style7")
             for p in allps:
-                alltext.append(p.get_text())
+                pdict = {"File":f, "Body":p.get_text(), "Class":1}
+                alltext += [pdict]
         return alltext
     
 class ComparisonTranscript():
-    def __init__(self, DirName):
+    def __init__(self, DirName, contentName="Ted"):
         self.DirName = DirName
+        self.contentName = contentName
     def get_transcript_text_tedx(self, filename):
         soup = BeautifulSoup(open(self.DirName+filename,encoding="ISO-8859-1"),features="lxml")
         allps = soup.find_all('p')
         alltext = []
         for p in allps:
-            alltext.append(p.get_text())
+            pdict = {"File":filename, "Body":p.get_text(), "Class":0}
+            alltext += [pdict]
         return alltext
-    def get_all_tedx(self):
+    def get_all_text(self):
         allfiles = os.listdir(self.DirName)
         alltext = []
         for f in allfiles:
-            alltext += self.get_transcript_text_tedx(f)
+            if self.contentName == "Ted":
+                alltext += self.get_transcript_text_tedx(f)
+            else:
+                pass
         return alltext
     
 class ComparisonBetween():
-    def __init__(self, comparisonObj, comparisonObjTxt):
-        self.it = CamelotConfTranscript()
+    def __init__(self, it, itTxt, comparisonObj, comparisonObjTxt):
+        self.it = it
         self.compareit = comparisonObj
-        self.it_txt = self.it.get_transcript_text("ProjectCamelotConference/")
+        self.it_txt = itTxt
         self.compare_txt = comparisonObjTxt
-    def make_df(self):
-        df = pd.DataFrame()
-        for p in self.it_txt:
-            #df2 = pd.DataFrame([[p,1]],columns=list('TextClass'))
-            print(type(p))
-            #df2.head()
-        return None
-        
+        self.df = self.make_df(self.it_txt, self.compare_txt)
+    def make_df(self, list1, list2):
+        df = pd.DataFrame(columns=['File','Body','Class'])
+        #print(df.head())
+        pccdf = pd.DataFrame(list1)
+        comparedf = pd.DataFrame(list2)
+        #print(pccdf.head())
+        #print(comparedf.head())
+        newdf = df.append(pccdf,ignore_index=True)
+        newnewdf = newdf.append(comparedf,ignore_index=True)
+        #print(newnewdf.head())
+        return newnewdf
+    
+def get_simple_test_set():
+    
+    compareit = ComparisonTranscript("PCCComparison/")
+    rawtext = compareit.get_all_text()
+    it = CamelotConfTranscript()
+    ittxt = it.get_transcript_text("ProjectCamelotConference/")
+
+    doit = ComparisonBetween(it, ittxt, compareit, rawtext)
+    #doit.df has all the training data in it
+    
+    testdf = pd.DataFrame(columns=["File","Body","Class"])
+    test_pc = CamelotConfTranscript()
+    pc_txt = test_pc.get_transcript_text("Test/PC/")
+    test_ted = ComparisonTranscript("Test/Ted/")
+    ted_txt = test_ted.get_all_text()
+    
+    testit = ComparisonBetween(test_pc,pc_txt,test_ted,ted_txt)
+    #testit.df has all the test data in it
+
+    train, val = train_test_split(doit.df, test_size=0.1, random_state=42)
+    #print(train)
+
+    vectorizer = CountVectorizer(stop_words="english", max_features=100)
+    X_train = vectorizer.fit_transform(train["Body"])
+    Y_train = train["Class"]
+    train_vocab = vectorizer.get_feature_names()
+    #print(train_vocab)
+    
+    #print([x for x in doit.df["Class"] if x not in (1,0)])
+#=============================================================================
+    model = LinearRegression().fit(X_train,Y_train)
+    
+    training_accuracy = model.score(X_train,Y_train)
+    print("Training Accuracy: ", training_accuracy)
+#=============================================================================
+    
+    val_vectorizer = CountVectorizer(stop_words="english", max_features=100, vocabulary=train_vocab)
+    X_val = val_vectorizer.fit_transform(val["Body"])
+    Y_val = val["Class"]
+    val_vocab = val_vectorizer.get_feature_names()
+    #print(val_vocab==train_vocab)
+    
+    val_accuracy = model.score(X_val,Y_val)
+    print("Validation Accuracy: ", val_accuracy)
+    
+    new_df = pd.DataFrame()
+    new_df["features"] = train_vocab
+    
+    new_df["weights"] = model.coef_[0]
+    ax = sns.distplot(new_df["weights"], kde=False)
+    ax.set_yscale('log')
+    
+    fig= plt.figure(figsize=(12,8))
+
+    sorteddf = new_df.sort_values(by="weights")
+    top_10_spam = sorteddf.iloc[0:10]
+    top_10_ham = sorteddf.iloc[-10:].iloc[::-1]
+    plotguy = pd.concat([top_10_spam,top_10_ham])
+    #print(plotguy)
+    
+    print("Words most associated with conspiracy:",top_10_spam)
+    print("Words most associated with tedx:",top_10_ham)
+    
+    sns.barplot(plotguy["weights"],plotguy["features"])
 
 
-compareit = ComparisonTranscript("PCCComparison/")
-rawtext = compareit.get_all_tedx()
+get_simple_test_set()
 
-doit = ComparisonBetween(compareit, rawtext)
-doit.make_df()
-
-
-# train = rawtext
-
-# vectorizer = CountVectorizer(stop_words="english", max_features=100)
-# X_train = vectorizer.fit_transform(train)
-# #Y_train = train["spam"]
-# train_vocab = vectorizer.get_feature_names()
-# print(train_vocab)
 
