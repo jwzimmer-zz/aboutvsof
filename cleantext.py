f51dbedc17859b3980873ea70deaa4a738969551
jwzimmer <jzimmer1@uvm.edu>
2021-01-21 15:33:48 -0500

    list whether files are of-q or about-q
    in q_filemap.json

--- cleantext.py
+++ cleantext.py
@@ -40,12 +40,48 @@ def get_json(filename):
         jsonobj = json.load(f)
     return jsonobj
     
-# === === === === === === === === 
+# === === === === === === === ===
 
-class QModel():
+def make_filemap():
+    dicti = {
+        "‘Depraved’_ Rep. Jason Crow condemns Rep. Marjorie Taylor Greene’s rhetoric ahead of impeachment vote - POLITICO.html":0,
+        "‘He knows he’s screwing with everyone’_ Gosar embraces conspiracy theories.htm":0,
+        "An overview of the “Out of Shadows” documentary – Est. 1933.html":1,
+        "Apple, Twitter, Google, and Instagram Collude to Defeat Trump _ Observer.htm":1,
+        "Creative Coalition Rejects $10k Donation From Veterans—Because ‘Trump’ _ Observer.htm":1,
+        "Does the Jeffrey Epstein indictment prove Qanon right_.htm":1,
+        "Donald Trump Laura Ingraham Interview Transcript August 31_ Says People _in the Dark Shadows_ Controlling Biden - Rev.htm":1,
+        "Exclusive_ Hillary Clinton Campaign Systematically Overcharging Poorest Donors _ _ Observer.htm":1,
+        "Facebook and YouTube Show Frantic Alliegence to Clinton _ Observer.htm":1,
+        "Fact check_ 35,000 “malnourished” and “caged” children were not recently rescued from tunnels by U.S. military _ Reuters.html":0,
+        "House votes to condemn baseless QAnon conspiracy theory - The Washington Post.htm":0,
+        "How QAnon Became Obsessed With ‘Adrenochrome,’ an Imaginary Drug Hollywood Is Harvesting from Kids.htm":0,
+        "Inside ‘Out of Shadows,’ the Bonkers Hollywood-Pedophilia Documentary QAnon Loves.htm":0,
+        "Is YouTube Preventing People From Finding The Out Of Shadows Documentary_ _ Small Screen.htm":1,
+        "Media Favoritism of Clinton Extends Beyond News Programs _ _ Observer.htm":1,
+        "Media Orgs Donate to Clinton Foundation Then Downplay Clinton Foundation Scandal _ _ Observer.htm":1,
+        "Milo Yiannopoulos Advises Donald Trump to Quit Twitter _ Observer.htm":1,
+        "National Enquirer Never Would Have Run BuzzFeed’s Trump Dossier _ Observer.htm":1,
+        "proqexcerpts.txt":1,
+        "QAnon and Child Trafficking_ Is 'Save the Children' a Conspiracy_.htm":0,
+        "QAnon Is More Important Than You Think - The Atlantic.htm":0,
+        "QAnon Promotes Pedo-Ring Conspiracy Theories. Now They’re Stealing Kids..html":0,
+        "QAnon_ the religion of conspiracy - Elcano Blog.htm":0,
+        "Shia LaBeouf’s Behavior at Trump Protest Is Hypocrisy at Its Finest _ Observer.htm":1,
+        "Team Trump Isn’t Hiding Support for QAnon Kooks Anymore After Marjorie Taylor Greene’s Win in Georgia Primary.htm":0,
+        "The Dark Virality of a Hollywood Blood-Harvesting Conspiracy _ WIRED.htm":0,
+        "Timothy Holmseth - Guest Profile.htm":1,
+        "Timothy Holmseth Update_ Deep State Harassment, Jury Trial _ Listen Notes.htm":1,
+        "Viral Chart Distorts Human Trafficking Statistics - FactCheck.org.html":0
+        }
+    write_json(dicti, "q_filemap.json")
+    return None
+
+make_filemap()
+
+class Text():
     def __init__(self):
-        self.df = self.get_df()
-        self.run_model(self.df)
+        pass
         
     def get_text_from_website(self,filename):
         soup = BeautifulSoup(open(self.DirName+filename,encoding="ISO-8859-1"),features="lxml")
@@ -54,77 +90,5 @@ class QModel():
         for p in allps:
             alltext += [p.get_text()]
         return alltext
-
-    def get_df(self,tokenlist0,tokenlist1,n=50):
-        #tokenlists should be json list objects (from a file) or python list objects
-        #0 for non-con, 1 for con; n is the chunk size (number of words in a data point)
-        #expected format = df has Body (text) and Class columns (0/1)
-        #df has all the training data in it
-        df = pd.DataFrame(columns=["Body","Class"])
-        newdf = []
-        chunks0 = [tokenlist0[i:i + n] for i in range(0, len(tokenlist0), n)]
-        chunks1 = [tokenlist1[i:i + n] for i in range(0, len(tokenlist1), n)]
-        for t in chunks0:
-            #print(t)
-            strt = ""
-            for i in t:
-                strt += " "+i
-            newdf += [{"Body":strt,"Class":0}]
-        for p in chunks1:
-            strp = ""
-            for i in p:
-                strp += " "+i
-            newdf += [{"Body":strp,"Class":1}]
-        df = df.append(pd.DataFrame(newdf),ignore_index=True)
-        #print(df.head())
-        return df
-        
-    def run_model(self,df):
     
-        train, val = train_test_split(df, test_size=0.2, random_state=42)
-        #print(train)
     
-        vectorizer = CountVectorizer(stop_words="english", max_features=10000)
-        X_train = vectorizer.fit_transform(train["Body"])
-        Y_train = train["Class"]
-        Y_train=Y_train.astype('int')
-        train_vocab = vectorizer.get_feature_names()
-        #print(train_vocab)
-        
-        #print([x for x in doit.df["Class"] if x not in (1,0)])
-    #=============================================================================
-        model = LogisticRegression().fit(X_train,Y_train)
-        
-        training_accuracy = model.score(X_train,Y_train)
-        print("Training Accuracy: ", training_accuracy)
-    #=============================================================================
-        
-        val_vectorizer = CountVectorizer(stop_words="english", max_features=10000, vocabulary=train_vocab)
-        X_val = val_vectorizer.fit_transform(val["Body"])
-        Y_val = val["Class"]
-        Y_val=Y_val.astype('int')
-        val_vocab = val_vectorizer.get_feature_names()
-        #print(val_vocab==train_vocab)
-        
-        val_accuracy = model.score(X_val,Y_val)
-        print("Validation Accuracy: ", val_accuracy)
-        
-        new_df = pd.DataFrame()
-        new_df["features"] = train_vocab
-        
-        new_df["weights"] = model.coef_[0]
-        ax = sns.distplot(new_df["weights"], kde=False)
-        ax.set_yscale('log')
-        
-        fig= plt.figure(figsize=(12,8))
-    
-        sorteddf = new_df.sort_values(by="weights")
-        top_10_spam = sorteddf.iloc[0:10]
-        top_10_ham = sorteddf.iloc[-10:].iloc[::-1]
-        plotguy = pd.concat([top_10_spam,top_10_ham])
-        #print(plotguy)
-
-        sns.barplot(plotguy["weights"],plotguy["features"])
-        
-
-QModel()
